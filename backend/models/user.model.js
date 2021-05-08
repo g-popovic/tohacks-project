445b67c824870b8913f9dcd0afceca844fe43e85
Grekus14 <georgepopovich14@gmail.com>
2021-05-08 20:26:56 +0200

    Add some routes

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,11 @@
+const mongoose = require('mongoose')
 
-
-const userSchema = new mongoose.schema({
-    email: {type: String, unique: true},
+const userSchema = new mongoose.Schema({
+    email: {type: String, unique: true,},
     password: String,
     city: String
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User; 
+module.exports = User;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,45 @@
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const router = require('express').Router();
 
+
+const User = require('../models/user.model.js')
 router.get('/test', (req, res) => {
 	res.send('bruh');
 });
 
 module.exports = router;
+
+router.post("/register", async  function(req,res){
+	const {email,password,city} = req.body;
+	const newuser = new User(
+	 {
+		email,
+		password: await bcrypt.hash(password, 10,)  ,
+        city
+
+	})
+});
+
+
+router.post("/login", async function(req,res){
+
+	const {password, email} = req.body;
+     User.findOne({email:email}, async 	 function(err, foundUser){
+        if(err){
+			res.sendStatus(500)
+		}else{
+			if (!foundUser) return res.status(401).send('Incorrect email')
+
+			 const isCorrect = await bcrypt.compare(password, foundUser.password)
+			if (isCorrect) {
+				req.session.user.id = foundUser.id;
+				res.sendStatus(200)
+			} else {
+				res.sendStatus(404)
+			}
+		}
+
+	 })
+
+});
