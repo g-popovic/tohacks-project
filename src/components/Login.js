933a7a50e40995e03e4f762b484bcc8f9adfb14c
George Popovic <georgepopovich14@gmail.com>
2021-05-08 22:56:13 +0200

    Hook up login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,61 @@
 import React, { useState } from 'react';
+import axiosApp from '../utils/axiosApp';
 import { countries } from '../utils/data';
 
 export default function Login() {
 	const [isRegister, setIsRegister] = useState(false);
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [country, setCountry] = useState('');
+
+	async function login() {
+		try {
+			await axiosApp.post('/login', { email, password });
+			window.location.reload();
+		} catch (err) {
+			alert(err.response ? err.response.message : 'Unexpected error');
+		}
+	}
+
+	async function register() {
+		try {
+			await axiosApp.post('/register', { email, password, country });
+			window.location.reload();
+		} catch (err) {
+			alert(err.response ? err.response.data : 'Unexpected error');
+		}
+	}
 
 	return (
 		<div className='login-container'>
 			<div className='login-panel rounded p-4 pb-3 shadow'>
 				<h1 className='text-center mb-3'>{isRegister ? 'Register' : 'Login'}</h1>
-				<input className='form-control my-3' placeholder='Email' />
-				<input className='form-control my-3' placeholder='Password' />
+				<input
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+					className='form-control my-3'
+					placeholder='Email'
+				/>
 				{isRegister && (
 					<select
+						value={country}
 						placeholder='Country'
 						className='form-select my-3'
-						onChange={(e, val) => console.log({ e: e.target.value, val })}>
+						onChange={e => setCountry(e.target.value)}>
 						{countries.map(el => (
 							<option value={el.code}>{el.name}</option>
 						))}
 					</select>
 				)}
-				<button className='btn btn-dark w-100 my-3'>
+				<input
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					className='form-control my-3'
+					placeholder='Password'
+				/>
+				<button
+					onClick={isRegister ? register : login}
+					className='btn btn-dark w-100 mt-3 mb-2'>
 					{isRegister ? 'Register' : 'Login'}
 				</button>
 				<p
