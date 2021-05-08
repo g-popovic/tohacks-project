47104d817ac5c1e40b6de3afb63dfeb7d1dc9e13
George Popovic <georgepopovich14@gmail.com>
2021-05-08 19:15:35 +0200

    Initialize backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const session = require('express-session');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24 * 30
+		}
+	})
+);
+
+app.use(routes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
