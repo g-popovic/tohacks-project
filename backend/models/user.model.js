1f276097afb9cc1001aae90a2ec2c6a927b88f7f
George Popovic <georgepopovich14@gmail.com>
2021-05-08 22:47:14 +0200

    Embed entries field into users model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
+
+const entrySchema = new mongoose.Schema(
+	{
+		activityId: { type: Number, required: true },
+		totalPoints: { type: Number, required: true },
+		units: { type: Number, required: true }
+	},
+	{ _id: false, timestamps: { updatedAt: false } }
+);
+
 const userSchema = new mongoose.Schema({
 	email: { type: String, unique: true, required: true },
 	password: { type: String, required: true },
 	country: { type: String, required: true },
-	entries: []
+	entries: [entrySchema]
 });
 
 const User = mongoose.model('User', userSchema);
