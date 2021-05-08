9b1e4d891c9d54b28ee8c40d3b29ee2872b60a30
George Popovic <georgepopovich14@gmail.com>
2021-05-08 19:43:59 +0200

    Make fields required

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,11 +1,9 @@
-
-
 const userSchema = new mongoose.schema({
-    email: {type: String, unique: true},
-    password: String,
-    city: String
+	email: { type: String, unique: true, required: true },
+	password: { type: String, required: true },
+	city: { type: String, required: true }
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User; 
+module.exports = User;
